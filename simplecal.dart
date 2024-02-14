b9c7e61e0fa447aa28b826c354c691ed92bc68cf
RushilBeladiya <rushilbeladiya8@gmail.com>
2024-02-14 13:41:35 +0530

    add simplecal.dart

---
+++ simplecal.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+void main() {
+  print("Enter the first number: ");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the second number: ");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the operation:-\n1.Addition,\n2.Subtraction,\n3.Multiplication,\n4.Divided");
+  String choice = stdin.readLineSync()!;
+
+  double result = 0;
+
+  switch (choice) {
+    case '1':
+      result = num1 + num2;
+      break;
+    case '2':
+      result = num1 - num2;
+      break;
+    case '3':
+      result = num1 * num2;
+      break;
+    case '4':
+      result = num1 / num2;
+      break;
+    default:
+      print("Error: Invalid operation");
+      return;
+  }
+  print("Result: $result");
+}
